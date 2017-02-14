3e65bfae4d00f877896ee67e1262fcd9f9c2139b
athompson909 <athompson909@gmail.com>
2017-02-13 17:32:38 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8083; // SETTING TO PORT 8083
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+	res.json({message: 'welcome to da api'});
+});
+
+// put more routes
+
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('starting on port ' + port);
