b61da2aa56d6409e0201e1d91947958a30427ddd
athompson909 <athompson909@gmail.com>
2017-02-18 10:10:06 -0700

    now allows cross origin requests

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 var port = process.env.PORT || 8083; // SETTING TO PORT 8083
 
 var router = express.Router();
